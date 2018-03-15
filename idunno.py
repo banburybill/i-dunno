6338adb77ae1068f90b70f1be7a600667e3e6e0b
Jim Hague <jim@sinodun.com>
2018-03-15 16:53:28 +0000

    Correct my horrid UTF-8 misconception.
    And check validity ranges in Python. They may be relevant on a final UTF-8 sequence.

--- idunno.py
+++ idunno.py
@@ -4,7 +4,7 @@ import argparse
 import ipaddress
 import unicodedata
 
-bits_per_char = [(0, 7), (0x80, 11), (0x800, 16), (0x10000, 21)]
+bits_per_char = [(0, 0x7f, 7), (0x80, 0x7ff, 11), (0x800, 0xffff, 16), (0x10000, 0x10ffff, 21)]
 
 def top_bits(bits, bitlen, nbits):
     if bitlen >= nbits:
@@ -20,11 +20,11 @@ def use_bits(bits, bitlen, nbits):
 def possible_next_char(bits, bitlen):
     res = []
     for b in bits_per_char:
-        c = b[0] + top_bits(bits, bitlen, b[1])
-        if c > 0 and c < 0x110000:
+        c = top_bits(bits, bitlen, b[2])
+        if c < 0x110000 and c >= b[0] and c <= b[1]:
             ch = chr(c)
             if unicodedata.category(ch)[0] in "LMNPSC":
-                res.append((ch, b[1]))
+                res.append((ch, b[2]))
     return res
 
 def print_idunno(id):
@@ -32,7 +32,7 @@ def print_idunno(id):
         printc = " "
         if unicodedata.category(c)[0] in "LNPS":
             printc = c
-        print('U+{:0=6X} {} {}'.format(ord(c), printc, unicodedata.name(c, "Unknown")))
+        print('U+{:0=6X} {} {} {}'.format(ord(c), unicodedata.category(c), printc, unicodedata.name(c, "Unknown")))
     print()
 
 def form_idunno_next(bits, bitlen, idunno):
